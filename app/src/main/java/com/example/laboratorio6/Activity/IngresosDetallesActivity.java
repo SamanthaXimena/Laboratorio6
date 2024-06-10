98ddf9fba6b81a11db527f068ddb8279baca14a5
Samantha Sanchez <94878885+SamanthaXimena@users.noreply.github.com>
2024-06-09 20:49:05 -0500

    modificacion 1

--- app/src/main/java/com/example/laboratorio6/Activity/IngresosDetallesActivity.java
+++ app/src/main/java/com/example/laboratorio6/Activity/IngresosDetallesActivity.java
@@ -2,24 +2,14 @@ package com.example.laboratorio6.Activity;
 
 import android.content.DialogInterface;
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
-import androidx.recyclerview.widget.RecyclerView;
-
-import com.bumptech.glide.Glide;
-import com.example.laboratorio6.Adapter.IngresosAdapter;
 import com.example.laboratorio6.R;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.firestore.FirebaseFirestore;
